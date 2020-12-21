e1cdcac86b959c237e1a265b03ec75e2879775d4
zhf <755109759@qq.com>
2020-12-21 22:41:10 +0800

    循环依赖

--- src/main/java/com/crayon/kk/injection/ClientTest.java
+++ src/main/java/com/crayon/kk/injection/ClientTest.java
@@ -1,5 +1,9 @@
 package com.crayon.kk.injection;
 
+/**
+ * DefaultSingletonBeanRegistry
+ * 通过这个类进行三级缓存解决循环依赖
+ */
 public class ClientTest {
     public static void main(String[] args) {
         InjectionServiceA a = new InjectionServiceA();
