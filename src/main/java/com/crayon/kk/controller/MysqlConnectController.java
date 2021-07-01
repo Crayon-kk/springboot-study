796c433e6e82ad11d5e6031512f2515d8e4c71aa
zhf <755109759@qq.com>
2021-07-01 17:22:22 +0800

    添加注释

--- src/main/java/com/crayon/kk/controller/MysqlConnectController.java
+++ src/main/java/com/crayon/kk/controller/MysqlConnectController.java
@@ -3,11 +3,13 @@ package com.crayon.kk.controller;
 import com.crayon.kk.entity.UserInfo;
 import com.crayon.kk.response.ResultResponse;
 import com.crayon.kk.service.impl.MysqlConnectServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 数据库连接测试类
+ *
  * @Author 张海锋
  * @Date 2021/6/29 15:24
  */
@@ -16,6 +18,7 @@ public class MysqlConnectController {
 
     private final MysqlConnectServiceImpl service;
 
+    @Autowired
     public MysqlConnectController(MysqlConnectServiceImpl service) {
         this.service = service;
     }
