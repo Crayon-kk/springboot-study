2baa244b4f07f73c63a3679c466c4c0e78301b04
zhf <755109759@qq.com>
2021-07-06 15:46:16 +0800

    值传递和引用传递

--- src/test/java/com/crayon/kk/PrintstacktraceTest.java
+++ src/test/java/com/crayon/kk/PrintstacktraceTest.java
@@ -1,27 +1,56 @@
 package com.crayon.kk;
 
-import java.util.concurrent.TimeUnit;
-
 /**
+ * 基本数据类型和创建新的引用类型的形参在方法中操作都不会改变实参的数据。 改变引用数据类型的某个值实参会同步该改变。
+ *
  * @Author 张海锋
  * @Date 2021/6/1 10:01
  */
 public class PrintstacktraceTest {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+        TestDemo demo = new TestDemo(1,"测试");
+        changeData(demo);
+        //TestDemo{code=11, name='测试'}
+        System.out.println(demo);
+    }
 
-        try {
-            printTest();
-        } catch (Exception e) {
-            e.printStackTrace();
-            TimeUnit.MINUTES.sleep(5);
+    private static void changeData(TestDemo demo) {
+        demo.setCode(11);
+        //The value new TestDemo(2,"测试2") assigned to 'demo' is never used
+        demo = new TestDemo(2,"测试2");
+    }
+    static class TestDemo{
+        private int code;
+        private String name;
 
+        public TestDemo(int code, String name) {
+            this.code = code;
+            this.name = name;
         }
 
+        public int getCode() {
+            return code;
+        }
 
-    }
-    public static void printTest() throws Exception {
+        public void setCode(int code) {
+            this.code = code;
+        }
 
-        throw new Exception("手动抛异常");
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "TestDemo{" +
+                    "code=" + code +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
     }
 }
