7d5087718a45d03354ceedd44b02c165119d19c8
zhf <755109759@qq.com>
2021-07-09 16:56:04 +0800

    默认生成一个2g的文件？（需要后续确认）

--- src/main/java/com/crayon/kk/mapdb/MapDbTest.java
+++ src/main/java/com/crayon/kk/mapdb/MapDbTest.java
@@ -4,6 +4,8 @@ import org.mapdb.DB;
 import org.mapdb.DBMaker;
 import org.mapdb.Serializer;
 
+import java.io.File;
+import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
 
 /**
@@ -12,13 +14,24 @@ import java.util.concurrent.ConcurrentMap;
  */
 public class MapDbTest {
     public static void main(String[] args) {
+//        DB db = DBMaker
+//                .fileDB("file.txt")
+//                .make();
+//        ConcurrentMap<String,Long> map = db
+//                .hashMap("map", Serializer.STRING, Serializer.LONG)
+//                .createOrOpen();
+        File fil = new File("file1.db");
+        System.out.println(fil.length());
         DB db = DBMaker
-                .fileDB("file.txt")
+                .fileDB("file.db")
+                .fileLockDisable()
+                .checksumHeaderBypass()
+                .closeOnJvmShutdown()
                 .make();
-        ConcurrentMap<String,Long> map = db
-                .hashMap("map", Serializer.STRING, Serializer.LONG)
+        ConcurrentMap<String,Long> allConsumerMap = db
+                .hashMap("map", Serializer.STRING, Serializer.JAVA)
                 .createOrOpen();
-        map.put("something", 111L);
-
+        allConsumerMap.entrySet().stream().forEach(System.out::println);
+        System.out.println(allConsumerMap.size());
     }
 }
